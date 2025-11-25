4968bd606f4bb0bc920a443399f69d0f5f764092
maitreyee7 <maitreyeepurohit7@gmail.com>
2025-11-25 13:35:00 +0530

    Initial Commit

---
+++ src/main/java/com/example/SbForGitApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbForGitApplication.class, args);
+	}
+
+}
